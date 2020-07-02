ffd205b4e68399fbfed784645682696868f96fb8
maui <maui@devf.mx>
2020-07-01 20:05:57 -0500

    Corregir texto de endpoint raiz

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3000;
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
 
-api.get('/', (req, res) => res.send('Helo World'));
+api.get('/', (req, res) => res.send('Hello World'));
 
 module.exports = { api, PORT };
