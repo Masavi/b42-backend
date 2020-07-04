c73bdae4770e6fbb6d052fc8f7a11dbdde2a3115
maui <maui@devf.mx>
2020-07-03 20:07:51 -0500

    Agregar validacion al endpoint de creacion de usuario

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const { UserValidator } = require('../validators');
 const { UserController } = require('../controllers');
 
 // ES6
-router.post('/api/v1/users', UserController.create);
+router.post('/api/v1/users', UserValidator.create, UserController.create);
 
 module.exports = router;
