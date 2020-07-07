7621463d37619350d7ca3499823783dcf637bd95
maui <maui@devf.mx>
2020-07-06 19:20:38 -0500

    Agregar validación para saber si un correo ya está en uso

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,10 @@ const { UserService } = require('../services');
 
 module.exports = {
   create: async (req, res) => {
+    const { email } = req.body;
     try {
+      const userExists = await UserService.findOneByEmail(email);
+      if (userExists) res.stats(400).json({ message: 'Email already taken' });
       const user = await UserService.create(req.body);
       res.status(201).json(user);
     } catch (err) {
@@ -31,6 +34,7 @@ module.exports = {
     const { body } = req;
     try {
       const user = await UserService.findOneById(id);
+      if (!user) res.status(404).json({ message: 'User not found' });
       const modifiedUser = await UserService.updateOne(user, body);
       res.status(200).json(modifiedUser);
     } catch (err) {
