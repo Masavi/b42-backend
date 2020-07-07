26480edd29e1b9a3f563079148daf1b8491a95c4
maui <maui@devf.mx>
2020-07-06 19:44:09 -0500

    Agregar enrutador de auth para manejo de signup y login

---
+++ routers/AuthRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { UserValidator } = require('../validators');
+const { UserController } = require('../controllers');
+
+router.post('/signup',
+  UserValidator.create, UserController.signup);
+
+module.exports = router;
